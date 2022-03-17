8bc2c23471e92f6d9e36fdbd702ea6c0d1da6ee2
ignacioiriondo <ignacio.ezequiel.iriondo@gmail.com>
2022-03-17 14:48:22 -0300

    config js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,7 +5,7 @@ module.exports= {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
-    "host": "mysql-ignacio-iriondo.alwaysdata.net",
+    "host": process.env.DB_HOST,
     "port": process.env.DB_PORT,
     "dialect": "mysql",
     "operatorsAliases": 0
